dc7d709df3d4ae4fd55912cc9ebdfb144978a775
a-mackay <amk@gegroup.com.au>
2020-11-22 10:55:45 +1100

    Remove categories, header and footer

--- src/App.js
+++ src/App.js
@@ -6,21 +6,9 @@ import { bgData } from "./Bgs.js";
 function App() {
     return (
         <div className="App">
-            <Header/>
             <BgTable/>
-            <Footer/>
         </div>
     );
-}
-
-function Header() {
-    return (
-        <div className="Header">
-            <div className="HeaderTitle">
-                We don't own enough boardgames
-            </div>
-        </div>
-    )
 }
 
 function BgTable() {
@@ -28,63 +16,24 @@ function BgTable() {
 
     return (
         <table className="BgTable">
-            <tr>
-                <th>Boardgame</th>
-                <th>Owner</th>
-            </tr>
-            {allCategories().map(cat => {
-                return <BgTableSection
-                    title={cat.name}
-                    iconClassName={cat.className}
-                    bgs={bgs.filter(bg => bg.complexity === cat.id)}
-                />
-            })}
+            <tbody>
+                <tr>
+                    <th>Boardgame</th>
+                    <th>Owner</th>
+                </tr>
+                {bgs.map((bg, index) => {
+                    return <BgRow
+                        key={bg.name}
+                        bg={bg}
+                        isOddRow={index % 2 !== 0}
+                    />
+                })}
+            </tbody>
         </table>
     )
 }
 
-function BgTableSection({title, iconClassName, bgs}) {
-    return (
-        <>
-            <tr className="BgTableSectionTitleRow">
-                <td colSpan="2">
-                    {BgTableSectionTitle(title, iconClassName)}
-                </td>
-            </tr>
-            {bgs.map((bg, index) => BgRow(bg, index % 2 === 0))}
-        </>
-    )
-}
-
-function BgTableSectionTitle(title, iconClassName) {
-    return (
-        <div className="BgTableSectionTitle">
-            {Icon(iconClassName)}
-            <div className="BgTableSectionTitleText">{title}</div>
-        </div>
-    )
-}
-
-function category(id, name, className) {
-    return {
-        "id": id,
-        "name": name,
-        "className": className
-    }
-}
-
-function allCategories() {
-    return [
-        category(5, "Gigantic Brain", "GiganticBrain"),
-        category(4, "Big Brained", "BigBrained"),
-        category(3, "Normal", "Normal"),
-        category(2, "Simpleton", "Simpleton"),
-        category(1, "Complete Pinhead", "CompletePinhead"),
-        category(0, "Shelf Of Shame", "ShelfOfShame")
-    ]
-}
-
-function BgRow(bg, isOddRow) {
+function BgRow({bg, isOddRow}) {
     let className = isOddRow ? "OddTableRow" : "EvenTableRow";
 
     return (
@@ -93,15 +42,6 @@ function BgRow(bg, isOddRow) {
             <td>{bg.owner}</td>
         </tr>
     )
-}
-
-function Footer() {
-    return <div className="Footer">Pioneering site design by a-mackay</div>
-}
-
-function Icon(imageClassName) {
-    let className = `Icon ${imageClassName}`;
-    return <div className={className}/>
 }
 
 export default App;
